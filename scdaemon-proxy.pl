e6527b578c2459d6b2428737ed0eb462d45b4d14
Klas Lindfors <klas@yubico.com>
2015-11-13 10:41:15 +0100

    handle stderr correctly, handle input closing

--- scdaemon-proxy.pl
+++ scdaemon-proxy.pl
@@ -5,55 +5,54 @@ use warnings;
 
 use IPC::Open3;
 use IO::Select;
+use Symbol;
 
 my $bin = "/usr/lib/gnupg2/scdaemon --multi-server";
 
-my $pid = open3(my $pipe_in, my $pipe_out, my $pipe_err, $bin) or die "failed to start $bin";
+sub logg($) {
+  my $msg = shift;
+  open(my $log, ">>", "/tmp/scdaemon-proxy.log");
+  print $log "$msg\n";
+  close $log;
+}
+
+my $pipe_err = gensym();
+my $pid = open3(my $pipe_in, my $pipe_out, $pipe_err, $bin) or die "failed to start $bin";
 my $s = IO::Select->new();
 $s->add(\*STDIN);
 $s->add($pipe_out);
-
-my $e = IO::Select->new();
 $s->add($pipe_err);
 
-open(my $log, ">>", "/tmp/scdaemon-proxy.log");
-print $log "starting\n";
-close $log;
-
-my $cryptop = 0;
+my $op = 0;
 
 while(1) {
   my @ready = $s->can_read(1);
-  if(!@ready and $cryptop) {
-    open(my $log, ">>", "/tmp/scdaemon-proxy.log");
-    print $log "notifying!\n";
-    close($log);
-    system("notify-send 'Crypto' 'Long running crypto operation'");
-    $cryptop = 0;
+  if(!@ready and $op) {
+    logg("notifying!");
+    system("notify-send 'Crypto' 'Long running sc-operation: $op'");
+    $op = undef;
   }
   foreach my $r (@ready) {
     my $message;
-    sysread($r, $message, 2048);
+    if(sysread($r, $message, 2048) == 0) {
+      logg("lost $r");
+      die("input closed.");
+    }
     foreach my $m (split(/\n/, $message)) {
-      open(my $log, ">>", "/tmp/scdaemon-proxy.log");
+      logg("$m on $r");
       $m .= "\n";
-      print $log $m;
-      close($log);
+      my $out;
       if($r eq \*STDIN) {
-        if($m =~ m/^pksign/i or $m =~ m/^pkdecrypt/i or $m =~ m/^auth/i) {
-          $cryptop = 1;
-        } else {
-          $cryptop = 0;
-        }
-        print $pipe_in $m;
+        $out = $pipe_in;
+        $m =~ m/^([a-zA-Z]+)/;
+        $op = $1;
       } elsif($r eq $pipe_out) {
-        $cryptop = 0;
-        if($m =~ m/^scdaemon/) {
-          print STDERR $m;
-        } else {
-          syswrite(STDOUT, $m);
-        }
+        $op = undef;
+        $out = \*STDOUT;
+      } elsif($r eq $pipe_err) {
+        $out = \*STDERR;
       }
+      syswrite($out, $m);
     }
   }
 }
